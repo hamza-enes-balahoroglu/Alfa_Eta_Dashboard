d21bd5aec79fc0c06c73a0abe2d1423851cd5801
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-06-21 23:58:35 +0300

    MapOffset tipi oluşturuldu.

--- Alfa_Eta_Dashboard/libs/Inc/geo_to_pixel.h
+++ Alfa_Eta_Dashboard/libs/Inc/geo_to_pixel.h
@@ -3,17 +3,24 @@
 
 #include "stm32f4xx_hal.h"
 
+/*--------------------- Map Location ---------------------*/
+#define MAP_X_MIN_VAL 0 // Minimum horizontal offset for the background image (fully left)
+#define MAP_X_MAX_VAL 450 // Maximum horizontal offset for the background image (fully right)
+#define MAP_Y_MIN_VAL -270 // Minimum vertical offset for the background image (fully up)
+#define MAP_Y_MAX_VAL 0 // Maximum vertical offset for the background image (fully down)
 
+/*--------------------- Geo Boundaries ---------------------*/
+#define NW_lat 40.824772493 // Latitude of the top-left (northwest) corner of the map
+#define NW_lon 29.417859918 // Longitude of the top-left (northwest) corner of the map
+#define SE_lat 40.822593887 // Latitude of the bottom-right (southeast) corner of the map
+#define SE_lon 29.420935394 // Longitude of the bottom-right (southeast) corner of the map
 
-#define MAP_SIZE_PX_X 800
-#define MAP_SIZE_PX_Y 750
-
-
-
-
-
-
-
+typedef struct {
+    int PixelX;
+    int PixelY;
+    int IconDirection;
+    int Lap;
+} MapOffset;
 
 
 #endif // GEO_TO_PIXEL
