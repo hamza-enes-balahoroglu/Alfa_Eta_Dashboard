c36d6b5dbd9b7464ca2cf944c25a981a34811cc5
Hamza Enes Balahoroğlu <hebalahoroglu@gmail.com>
2025-06-22 16:07:07 +0300

    GPS verisi okundu. Float kullanımından kaynaklı bir sorun oluştuğuna inanıyorum. Gelen veriler saçma şekilde büyük.

--- Alfa_Eta_Dashboard/libs/Inc/geo_to_pixel.h
+++ Alfa_Eta_Dashboard/libs/Inc/geo_to_pixel.h
@@ -2,6 +2,17 @@
 #define GEO_TO_PIXEL
 
 #include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAP_X_SIZE 800
+#define MAP_Y_SIZE 480
+
+#define ICON_WIDTH  67
+#define ICON_HEIGHT 67
+#define ICON_X 640
+#define ICON_Y 201
 
 /*--------------------- Map Location ---------------------*/
 #define MAP_X_MIN_VAL 0 // Minimum horizontal offset for the background image (fully left)
@@ -10,17 +21,39 @@
 #define MAP_Y_MAX_VAL 0 // Maximum vertical offset for the background image (fully down)
 
 /*--------------------- Geo Boundaries ---------------------*/
-#define NW_lat 40.824772493 // Latitude of the top-left (northwest) corner of the map
-#define NW_lon 29.417859918 // Longitude of the top-left (northwest) corner of the map
-#define SE_lat 40.822593887 // Latitude of the bottom-right (southeast) corner of the map
-#define SE_lon 29.420935394 // Longitude of the bottom-right (southeast) corner of the map
+#define NW_lat 40.824772493 // Latitude (vertical) of the top-left (northwest) corner of the map
+#define NW_lon 29.417859918 // Longitude (horizontal) of the top-left (northwest) corner of the map
+#define SE_lat 40.822593887 // Latitude (vertical) of the bottom-right (southeast) corner of the map
+#define SE_lon 29.420935394 // Longitude (horizontal) of the bottom-right (southeast) corner of the map
+
+#define GPS_BUFFER_SIZE 100
 
 typedef struct {
     int PixelX;
     int PixelY;
-    int IconDirection;
+    int IconAngle;
     int Lap;
 } MapOffset;
 
+
+void Test(char *buf);
+
+
+
+void Run_GeoPipeline(void);
+
+void Geo_To_Pixel_Init(UART_HandleTypeDef *uart, MapOffset *mapData);
+
+void Read_GPS_Location(void);
+
+float NMEA_To_Decimal(char *nmea, char hemisphere);
+
+void Calculate_Geo_To_Pixel(void);
+
+void Get_Map_Draw_Position(int gpsPixelX, int gpsPixelY);
+
+void Calculate_Icon_Angle(void);
+
+float Map_Float(float input, float in_min, float in_max, float out_min, float out_max);
 
 #endif // GEO_TO_PIXEL
--- Alfa_Eta_Dashboard/libs/Src/geo_to_pixel.c
+++ Alfa_Eta_Dashboard/libs/Src/geo_to_pixel.c
@@ -1,5 +1,147 @@
 #include "geo_to_pixel.h"
-#include "dashboard_controls.h"
 
-const int MAP_X_MIN_VAL = 0;
-const int MAP_X_MAX_VAL = NEX_S;
+static UART_HandleTypeDef *_uart = NULL;
+
+char gps_buffer[GPS_BUFFER_SIZE];
+
+static MapOffset *_mapData = NULL;
+static MapOffset _mapCachedData = {
+		.PixelX		=	0,
+		.PixelY		=	0,
+		.IconAngle 	=	0,
+		.Lap 		=	0,
+};
+
+float actualLon, actualLat;
+
+void Geo_To_Pixel_Init(UART_HandleTypeDef *uart, MapOffset *mapData){
+    _uart = uart;
+    _mapData = mapData;
+}
+
+void Run_GeoPipeline(void){
+	Read_GPS_Location();
+
+	Calculate_Geo_To_Pixel();
+
+	if (_mapCachedData.PixelX != _mapData->PixelX || _mapCachedData.PixelY != _mapData->PixelY){
+		Calculate_Icon_Angle();
+		_mapCachedData.PixelX = _mapData->PixelX;
+		_mapCachedData.PixelY = _mapData->PixelY;
+	}
+	// _mapData->Lap = _mapData->Lap+1;
+
+}
+
+static float *_lon = NULL;
+static float *_lat = NULL;
+static char *_buf = NULL;
+void Test(char *buf){
+	_buf = buf;
+}
+
+void Read_GPS_Location(void) {
+	float latitude,longitude;
+    memset(gps_buffer, 0, GPS_BUFFER_SIZE);
+    HAL_UART_Receive(_uart, (uint8_t *)gps_buffer, GPS_BUFFER_SIZE, 1000);
+    _buf = gps_buffer;
+    if (strstr(gps_buffer, "$GPGGA")) {
+        char *token;
+        int field = 0;
+
+        token = strtok(gps_buffer, ",");
+        while (token != NULL) {
+            field++;
+
+            if (field == 3) {
+                latitude = NMEA_To_Decimal(token, 'N'); // default 'N'
+            }
+            else if (field == 4) {
+                if (token[0] == 'S')
+                    latitude = -latitude;
+            }
+            else if (field == 5) {
+            	longitude = NMEA_To_Decimal(token, 'E'); // default 'E'
+            }
+            else if (field == 6) {
+                if (token[0] == 'W')
+                    longitude = -longitude;
+            }
+
+            token = strtok(NULL, ",");
+        }
+        actualLat = latitude;
+        actualLon = longitude;
+        if (_lat != NULL) *_lat = latitude;
+        if (_lon != NULL) *_lon = longitude;
+    }
+}
+
+float NMEA_To_Decimal(char *nmea, char hemisphere) {
+    float deg, min;
+    float val = atof(nmea);
+    deg = (int)(val / 100);
+    min = val - (deg * 100);
+    float decimal = deg + (min / 60.0);
+    if (hemisphere == 'S' || hemisphere == 'W')
+        decimal = -decimal;
+    return decimal;
+}
+
+void Calculate_Geo_To_Pixel(void){
+	float mappedXf = Map_Float(actualLon, NW_lon, SE_lon, 0.00f , MAP_X_SIZE);
+	int mappedX = (int)mappedXf;
+
+	float mappedYf = Map_Float(actualLat, NW_lat, SE_lat, 0.00f, MAP_Y_SIZE);
+	int mappedY = (int)mappedYf;
+
+	Get_Map_Draw_Position(mappedX, mappedY);
+}
+
+void Get_Map_Draw_Position(int gpsPixelX, int gpsPixelY) {
+	int pixelX, pixelY;
+
+	pixelX = ICON_X + (ICON_WIDTH/2) - gpsPixelX;
+/*
+	if(pixelX < MAP_X_MIN_VAL)
+		pixelX = MAP_X_MIN_VAL;
+	else if(pixelX > MAP_X_MAX_VAL)
+		pixelX = MAP_X_MAX_VAL; */
+
+	pixelY = ICON_Y + (ICON_HEIGHT/2) - gpsPixelY;
+
+/*	if(pixelY < MAP_Y_MIN_VAL)
+		pixelY = MAP_Y_MIN_VAL;
+	else if(pixelY > MAP_Y_MAX_VAL)
+		pixelY = MAP_Y_MAX_VAL; */
+	_mapData->PixelX = pixelX;
+	_mapData->PixelY = pixelY;
+}
+
+void Calculate_Icon_Angle(void) {
+
+    double angle_rad = atan2(_mapCachedData.PixelY - _mapData->PixelY, _mapCachedData.PixelX - _mapData->PixelX);  // -π ile +π arası
+    double angle_deg = angle_rad * (180.0 / M_PI);             // -180 ile +180 arası
+
+    angle_deg += 180;                                          // 0 derece batı olacak şekilde kaydırıyoruz
+
+    // 0-360 arası normalize et
+    if (angle_deg < 0) angle_deg += 360;
+    if (angle_deg >= 360) angle_deg -= 360;
+
+    _mapData->IconAngle = angle_deg;  // artık 0 (batı) ile 360 arası
+}
+
+/*
+float Map_Float(float input, float in_min, float in_max, float out_min, float out_max) {
+    float ratio = (input - in_min) / (in_max - in_min);
+    return out_min + ratio * (out_max - out_min);
+}
+*/
+float Map_Float(float input, float in_min, float in_max, float out_min, float out_max)
+{
+    float input_range = in_max - in_min + 1;
+    float ref_range = out_max - out_min + 1;
+
+    return ((ref_range * (input - in_min)) / input_range) + out_min;
+}
